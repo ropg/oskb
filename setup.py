9d1bacca893106f60a1c1c56b0b3bc103d39bde0
Rop <rop@gonggri.jp>
2020-03-02 22:35:05 +0100

    0.2.1 release

--- setup.py
+++ setup.py
@@ -9,20 +9,26 @@ this_directory = path.abspath(path.dirname(__file__))
 
 setup(
     name="oskb",
-    version="0.2.0",
+    version="0.2.1",
     description="On-Screen KeyBoard",
     long_description="I disliked the on-screen keyboards for Linux I tried. So I wrote a new one. Brand new but maturing fast... Please check the website for screenshots and instructions",
     url="https://github.com/ropg/oskb",
     author="Rop Gonggrijp",
     license="MIT",
-    classifiers=["Development Status :: 3 - Alpha", "Programming Language :: Python :: 3",],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+    ],
     keywords="keyboard",
     project_urls={
         "Documentation": "https://github.com/ropg/oskb/README.md",
         "Source": "https://github.com/ropg/oskb",
         "Tracker": "https://github.com/ropg/oskb/issues",
     },
-    packages=["oskb", "oskb.im",],
+    packages=[
+        "oskb",
+        "oskb.im",
+    ],
     python_requires=">=3",
     setup_requires=["wheel"],
     install_requires=[
@@ -37,5 +43,11 @@ setup(
             "oskbedit = oskb.oskbedit:main",
         ],
     },
-    package_data={"oskb": ["keyboards/*", "*.css", "ui/*",]},
+    package_data={
+        "oskb": [
+            "keyboards/*",
+            "*.css",
+            "ui/*",
+        ]
+    },
 )
